75d7df9074247734a011ed126c0fcbc3cebe5c38
Simon Pannek <simon.pannek@tum.de>
2021-05-12 22:36:30 +0200

    Add cheatsheet command

---
+++ commands/grnvs/cheatsheet.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "cheatsheet",
+    description: "Print a link to the cheat sheet",
+    async execute() {
+        return "https://svm0000.net.in.tum.de/altklausuren/cheatsheet_2020.pdf";
+    }
+};

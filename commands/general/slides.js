a377cfca4cbbfaf71c19ce163b86676c82c70c1a
Rafael Brandmaier <rafael.brandmaier@tum.de>
2021-05-23 22:56:04 +0200

    remove duplicate slides command

--- commands/general/slides.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "slides",
-    description: "Print a link to the tutorial slides, if available",
-    async execute() {
-        return "The slides can be downloaded directly in BBB at the bottom-left corner.";
-    }
-};

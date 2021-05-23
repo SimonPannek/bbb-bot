42d846e6761dc6e63f20a38bf974017ea237dbaf
Rafael Brandmaier <rafael.brandmaier@tum.de>
2021-05-23 22:49:14 +0200

    Add slides command

---
+++ commands/general/slides.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "slides",
+    description: "Print a link to the tutorial slides, if available",
+    async execute() {
+        return "The slides can be downloaded directly in BBB at the bottom-left corner.";
+    }
+};

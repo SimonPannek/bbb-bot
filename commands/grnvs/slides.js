c5660dfa67991387b16d421e435e136795a7cb39
Simon Pannek <simon.pannek@tum.de>
2021-05-23 16:36:25 +0200

    Add slides command

---
+++ commands/grnvs/slides.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "slides",
+    description: "Mention where to find the slides.",
+    async execute() {
+        return "You can download the slides in the bottom left.";
+    }
+};

1436eeff6640315073a0e9fefa7d3631ba36d6c1
Simon Pannek <simon.pannek@tum.de>
2021-05-24 10:15:53 +0200

    Minor corrections to the klausurrelevant command

--- commands/general/klausurrelevant.js
+++ commands/general/klausurrelevant.js
@@ -1,12 +1,10 @@
-const limit = 3;
-var tries = 0;
+let tries = 0;
 
 module.exports = {
     name: "klausurrelevant",
-    description: "Answer the age old question whether a topic is klausurrelevant",
+    description: "Answer the age old question whether a topic is \"klausurrelevant\".",
     async execute() {
-        tries += 1; // if this overflows the sutdents have already won.
-        
-        return "Yes."+ (tries >= limit?" And to be honest, this is a really stupid question.":"");
+        tries++;
+        return "Yes." + (tries % 3 === 0 ? " And to be honest, this is a really stupid question." : "");
     }
 };

340d34e724a3c2f35016a092cf3d2f44f13b6acf
Robin Marchart <robin.marchart@hotmail.com>
2021-06-29 17:34:26 +0200

    fix description

--- commands/general/discord.js
+++ commands/general/discord.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "discord",
-    description: "Send link to the most excellent of discord servers",
+    description: "Send link to the most excellent of discord servers.",
     async execute() {
         return "The essence of excellence: https://discord.gg/tum";
     }

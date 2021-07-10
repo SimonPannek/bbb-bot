e055ed607230aa4e542261b442a2ebbd526b8dc2
Rafael Brandmaier <rafael.brandmaier@tum.de>
2021-07-10 22:26:51 +0200

    add contact command

---
+++ commands/general/contact.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "contact",
+    description: "Where to contact your tutor.",
+    async execute() {
+        return "E-Mail: name@example.com\nZulip: John Doe\nMatrix: gt42qed@tum.de”";
+    }
+};
